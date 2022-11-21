64081e6c47fb586fa146826a6fdf803950b0c6ae
mrskeletone <91458603+mrskeletone@users.noreply.github.com>
2022-11-21 14:45:10 +0300

    Delete src/lab4 directory

--- src/lab4/task2/TShirt.java
+++
@@ -1,8 +0,0 @@
-package lab4.task2;
-
-public class TShirt extends  Clothes{
-
-    public TShirt(double size, double cost, char color) {
-        super(size, cost, color);
-    }
-}
--- src/lab4/task2/Tie.java
+++
@@ -1,7 +0,0 @@
-package lab4.task2;
-
-public class Tie extends Clothes{
-    public Tie(double size, double cost, char color) {
-        super(size, cost, color);
-    }
-}
