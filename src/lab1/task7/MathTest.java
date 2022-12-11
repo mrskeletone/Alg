fd145b18b13cf38cb35f7b0d9281f0abb9037b5e
mrskeletone <91458603+mrskeletone@users.noreply.github.com>
2022-12-11 23:19:10 +0300

    Add files via upload

---
+++ src/lab1/task7/MathTest.java
@@ -0,0 +1,13 @@
+package lab1.task7;
+import java.util.Scanner;
+
+import static num7.Math.factorial;
+
+public class MathTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length;
+        length = sc.nextInt();
+        factorial(length);
+    }
+}
